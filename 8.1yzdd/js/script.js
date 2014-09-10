ee175c18ba56e18f543904ad86f1f59a37026009
zwb周文彬 <wbzhou@ctrip.com>
2014-09-10 09:14:33 +0800

    add

--- 8.1yzdd/js/script.js
+++ 8.1yzdd/js/script.js
@@ -1,4 +1,5 @@
 $(function(){
+  $("body").addClass("ani1");
   $(".buttons").on("click","button",function(){
     var index=$("button").index(this)+1;
     $("body").removeClass().addClass("ani"+index);
